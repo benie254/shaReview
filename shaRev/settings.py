c69d1aa41c9eb10536788b92dbf1dce5fd1ec6e4
Benie <davinci.monalissa@gmail.com>
2022-06-13 20:09:25 +0300

    define default authentication classes for the rest framework package

--- shaRev/settings.py
+++ shaRev/settings.py
@@ -89,6 +89,12 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
